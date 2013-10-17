357c7932da8b4407e4ad64921377099d550e199c
georgeee <george.agapov@gmail.com>
2013-10-17 05:07:04 +0400

    Fixed bug with windows-1251 xml declaration encoding

--- src/ru/georgeee/android/gfeedreader/utility/http/XmlResponseHttpTask.java
+++ src/ru/georgeee/android/gfeedreader/utility/http/XmlResponseHttpTask.java
@@ -1,23 +1,26 @@
 package ru.georgeee.android.gfeedreader.utility.http;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
 import ru.georgeee.android.gfeedreader.utility.xml.SAXHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created with IntelliJ IDEA.
@@ -41,8 +44,22 @@ public abstract class XmlResponseHttpTask<Result> extends HttpTask<Result> {
     protected void handleSAXException(SAXException ex) {
         ex.printStackTrace();
     }
+
     protected void handleParserConfigurationException(ParserConfigurationException ex) {
         ex.printStackTrace();
+    }
+
+    private String getEncondingFromEntity(HttpEntity entity) {
+        if (entity.getContentType() != null) {
+            //Content-Type: text/xml; charset=ISO-8859-1
+            //Content-Type: text/xml; charset=UTF-8
+            for (String str : entity.getContentType().getValue().split(";")) {
+                if (str.toLowerCase().contains("charset")) {
+                    return str.toLowerCase().replace("charset=", "").replace(";", "").replace(" ", "");
+                }
+            }
+        }
+        return null;
     }
 
     @Override
@@ -54,7 +71,15 @@ public abstract class XmlResponseHttpTask<Result> extends HttpTask<Result> {
             SAXParser saxParser = spf.newSAXParser();
             XMLReader xmlReader = saxParser.getXMLReader();
             xmlReader.setContentHandler(handler);
-            xmlReader.parse(new InputSource(httpResponse.getEntity().getContent()));
+            InputStreamWrapper isw = new InputStreamWrapper(httpResponse.getEntity().getContent());
+            String encoding = isw.getEncoding();
+            if (encoding == null) {
+                encoding = getEncondingFromEntity(httpResponse.getEntity());
+            }
+            Reader isr = encoding == null ? new InputStreamReader(isw) : new InputStreamReader(isw, Charset.forName(encoding));
+            InputSource is = new InputSource();
+            is.setCharacterStream(isr);
+            xmlReader.parse(is);
             return handler.getResult();
         } catch (SAXException e) {
             handleSAXException(e);
@@ -62,6 +87,53 @@ public abstract class XmlResponseHttpTask<Result> extends HttpTask<Result> {
             handleParserConfigurationException(e);
         }
         return null;
+    }
+
+    /**
+     * Class for extracting encoding from xml declaration
+     * SAX Parser fails to do it
+     */
+    static class InputStreamWrapper extends InputStream {
+        InputStream is;
+        String encoding = null;
+        ArrayList<Byte> firstLineBytes = new ArrayList<Byte>();
+        int counter = 0;
+
+        InputStreamWrapper(InputStream is) throws IOException {
+            this.is = is;
+            int _byte;
+            while ((_byte = is.read()) != -1) {
+                if ((char) _byte == '\n') {
+                    //<?xml version="1.0" encoding="utf-8"?>
+                    byte[] bytes = new byte[firstLineBytes.size()];
+                    for (int i = 0; i < bytes.length; ++i) {
+                        bytes[i] = firstLineBytes.get(i);
+                    }
+                    String xmlHeader = new String(bytes);
+                    String expr = "<\\?xml.*encoding\\=\"([^\"]+)\".*\\?>";
+                    Pattern p = Pattern.compile(expr);
+                    Matcher m = p.matcher(xmlHeader);
+                    if (m.find()) {
+                        encoding = m.group(1);
+                        xmlHeader = xmlHeader.replaceFirst(expr, "");
+                        firstLineBytes.clear();
+                        for(byte sbyte : xmlHeader.getBytes()) firstLineBytes.add(sbyte);
+                    }
+                    break;
+                } else firstLineBytes.add((byte) _byte);
+            }
+        }
+
+        String getEncoding() {
+            return encoding;
+        }
+
+        @Override
+        public int read() throws IOException {
+            if (counter < firstLineBytes.size()) return firstLineBytes.get(counter++);
+            return is.read();
+        }
+
     }
 
 }
